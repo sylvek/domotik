95ca1b3017a7aa1c59581365424b2511ab6d324a
Sylvek <smaucourt@gmail.com>
2015-06-30 16:10:47 +0200

    doublon

--- cron_cc128.sh
+++ cron_cc128.sh
@@ -6,7 +6,4 @@ sleep 15 &&
 /home/pi/domotik/mosquitto_sub_sensors_temp_cc128.sh &&
 sleep 15 &&
 /home/pi/domotik/mosquitto_sub_sensors_watt_cc128.sh &&
-/home/pi/domotik/mosquitto_sub_sensors_temp_cc128.sh &&
-sleep 15 &&
-/home/pi/domotik/mosquitto_sub_sensors_watt_cc128.sh &&
-/home/pi/domotik/mosquitto_sub_sensors_temp_cc128.sh
+/home/pi/domotik/mosquitto_sub_sensors_temp_cc128.sh 
