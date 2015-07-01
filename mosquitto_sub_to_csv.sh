e31711931fb9113ba3fc1cd6bb205920377a405c
Sylvek <smaucourt@gmail.com>
2015-07-01 10:24:09 +0200

    fix date bug

--- mosquitto_sub_to_csv.sh
+++ mosquitto_sub_to_csv.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-DAY=$(date +"%Y-%m-%d %H:%M:%S")
-mosquitto_sub -v -t sensors/# | while IFS= read -r line; do echo "$DAY $line"; done >> export.csv &
+mosquitto_sub -v -t sensors/# | while IFS= read -r line; do echo "$(date +"%Y-%m-%d %H:%M:%S") $line"; done >> export.csv &
