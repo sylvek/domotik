2ad331168eedcf4b01aa6fb9464120edd75edeed
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-03 13:58:33 +0200

    refacto. sur les noms des services

--- mosquitto_pub_sensors_watt_cc128.sh
+++ mosquitto_pub_sensors_watt_cc128.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/pi/domotik/measureItRetriever.py http://192.168.0.2/measureit/measureit_public_html watt | mosquitto_pub -t sensors/cc128/watt -l
+/home/pi/domotik/service_measureItRetriever.py http://192.168.0.2/measureit/measureit_public_html watt | mosquitto_pub -t sensors/cc128/watt -l
