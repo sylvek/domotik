8c69c037f0299e9a3cf8cfdf30624ca10e332ae5
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-04 15:14:30 +0200

    thinkspeak ..:-|

--- mosquitto_sub_to_thinkspeak.sh
+++ mosquitto_sub_to_thinkspeak.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-mosquitto_sub -t sensors/cc128/temp | ./client-thinkspeak.py SMSXCZZDKPNH3HJ5 1 &
-mosquitto_sub -t sensors/cc128/watt | ./client-thinkspeak.py SMSXCZZDKPNH3HJ5 2 &
-mosquitto_sub -t sensors/juvisy/temp | ./client-thinkspeak.py SMSXCZZDKPNH3HJ5 3 &
-mosquitto_sub -t sensors/pi/temp | ./client-thinkspeak.py SMSXCZZDKPNH3HJ5 4 &
-mosquitto_sub -t sensors/juvisy/wind | ./client-thinkspeak.py SMSXCZZDKPNH3HJ5 5 &
+mosquitto_sub -t sensors/cc128/temp | ./client-thinkspeak.py <<api>> <<field>> &
