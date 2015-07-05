285bbc14c46c8bf1c5fd32ae3422a74a8aebcbe1
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-05 11:27:29 +0200

    full path

--- mosquitto_sub/graphite.sh
+++ mosquitto_sub/graphite.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mosquitto_sub -v -t sensors/# | ../clients/client-graphite.py &
+mosquitto_sub -v -t sensors/# | /home/pi/domotik/clients/client-graphite.py &
--- mosquitto_sub/mongodb.sh
+++ mosquitto_sub/mongodb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mosquitto_sub -v -t sensors/# | ../clients/client-mongodb.py &
+mosquitto_sub -v -t sensors/# | /home/pi/domotik/clients/client-mongodb.py &
