55eb49b36999a1a1082e69f3dc05c65e096116fe
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-07 16:31:07 +0200

    add auto refresh

--- web/public/javascripts/domotik-app.js
+++ web/public/javascripts/domotik-app.js
@@ -13,7 +13,7 @@
 
   });
 
-  app.controller('domotikIndexCtrl', function($scope, domotikSrv){
+  app.controller('domotikIndexCtrl', function($scope, $interval, domotikSrv){
       domotikSrv.last("24h", "temp").then(function(response) {
         $scope.twenty_four_hours_temp = response.data;
       });
@@ -21,6 +21,38 @@
       domotikSrv.last("24h", "watt").then(function(response) {
         $scope.twenty_four_hours_watt = response.data;
       });
+
+      function update() {
+        domotikSrv.last("20s", "temp").then(function(response) {
+          response.data.forEach(function(e1) {
+            var d = $scope.twenty_four_hours_temp;
+            d.forEach(function(e2) {
+                if (e2.key == e1.key) {
+                  e1.values.forEach(function(e3) {
+                    e2.values.push(e3);
+                    $scope.last_temp = e1.key + " " + e3[1] + "°c";
+                  });
+                }
+            });
+            $scope.twenty_four_hours_temp = d;
+          });
+        });
+        domotikSrv.last("20s", "watt").then(function(response) {
+          response.data.forEach(function(e1) {
+            var d = $scope.twenty_four_hours_watt;
+            d.forEach(function(e2) {
+                if (e2.key == e1.key) {
+                  e1.values.forEach(function(e3) {
+                    e2.values.push(e3);
+                    $scope.last_watt = e1.key + " " + e3[1] + "W";
+                  });
+                }
+            });
+            $scope.twenty_four_hours_watt = d;
+          });
+        })
+      }
+      setInterval(function() {$scope.$apply(update);}, 20000 /* 20S */);
 
       $scope.xAxisTickFormat_Date_Format = function() {
         return function(d) {
@@ -39,7 +71,7 @@
       }
       $scope.yAxisTickFormat_Watt_Format = function() {
         return function(d) {
-          return d + " watt";
+          return d + "W";
         }
       }
   });
