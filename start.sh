44b76ac57489b34ae01021b0648b3666aca8e0d9
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-07 22:20:46 +0200

    more explicit

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo "launching mosquitto subscribing..."
 cd $HOME/domotik/mosquitto_sub
 ./syslog.sh
 ./mongodb.sh
-echo "launching web interface"
+echo "launching web interface..."
 cd $HOME/domotik/web
 npm install
 node_modules/bower/bin/bower install
--- stop.sh
+++ stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "stopping mosquitto subscribing..."
 killall mosquitto_sub
+echo "stopping web interface..."
 cd $HOME/domotik/web
 npm stop
+echo "bye"
