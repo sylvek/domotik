6c95dd3c66bf17178eccd600a019a8149ff617a8
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-08 22:59:14 +0200

    Oo

--- mosquitto_pub/jpg_webcam.sh
+++ mosquitto_pub/jpg_webcam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CAPTURE=/tmp/capture.jpg
 $HOME/domotik/services/service-captureCamera.sh $CAPTURE
-base64 -w 0 $CAPTURE | mosquitto_sub -t sensors/camera/jpg -s
+base64 -w 0 $CAPTURE | mosquitto_pub -t sensors/camera/jpg -s
