fd237bcbb0b91a5dd11e5d568009832a32382867
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-10 07:37:25 +0200

    triggers instead of sensors

--- mosquitto_sub/freebox.sh
+++ mosquitto_sub/freebox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 FREEBOX="http://hd1.freebox.fr/pub/remote_control?code=3976946&key="
-mosquitto_sub -t sensors/+/menu | xargs -i curl -s -L "${FREEBOX}power" &
-mosquitto_sub -t sensors/+/play | xargs -i curl -s -L "${FREEBOX}ok" &
-mosquitto_sub -t sensors/+/plus | xargs -i curl -s -L "${FREEBOX}vol_inc" &
-mosquitto_sub -t sensors/+/minus | xargs -i curl -s -L "${FREEBOX}vol_dec" &
-mosquitto_sub -t sensors/+/forward | xargs -i curl -s -L "${FREEBOX}prgm_inc" &
-mosquitto_sub -t sensors/+/rewind | xargs -i curl -s -L "${FREEBOX}prgm_dec" &
+mosquitto_sub -t triggers/+/menu | xargs -i curl -s -L "${FREEBOX}power" &
+mosquitto_sub -t triggers/+/play | xargs -i curl -s -L "${FREEBOX}ok" &
+mosquitto_sub -t triggers/+/plus | xargs -i curl -s -L "${FREEBOX}vol_inc" &
+mosquitto_sub -t triggers/+/minus | xargs -i curl -s -L "${FREEBOX}vol_dec" &
+mosquitto_sub -t triggers/+/forward | xargs -i curl -s -L "${FREEBOX}prgm_inc" &
+mosquitto_sub -t triggers/+/rewind | xargs -i curl -s -L "${FREEBOX}prgm_dec" &
