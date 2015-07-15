2ef3f4b16765033056cd5b2819909cc5c2e27ca0
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-15 23:20:21 +0200

    ensureIndex

--- web/routes/api.js
+++ web/routes/api.js
@@ -8,23 +8,25 @@ router.get('/last/:time(24h|20s)/:type(temp|watt)', function(req, res, next) {
   var twenty_seconds = Math.floor((new Date().getTime() - 20*1000) / 1000);
   var timestamp = ("24h" == req.params.time) ? twenty_four_hours : twenty_seconds;
 
-  collection.find({ type:req.params.type, timestamp:{$gt: timestamp} },
-                  { fields: {timestamp:1, sensor:1, value:1, _id:0},
-                    sort: {timestamp:1} }, function (err, elements) {
+  collection.ensureIndex({ type:1, timestamp:1 }, { background:true }, function(err, indexName) {
+    collection.find({ type:req.params.type, timestamp:{$gt: timestamp} },
+                    { fields: {timestamp:1, sensor:1, value:1, _id:0},
+                      sort: {timestamp:1} }, function (err, elements) {
 
-        var result = [];
-        var type = [];
-        elements.forEach(function(entry){
-            var index = type.indexOf(entry.sensor);
-            if (index == -1) {
-              type.push(entry.sensor);
-              result.push({"key": entry.sensor, "values":[]});
-              index = type.length - 1;
-            }
-            result[index].values.push([entry.timestamp, entry.value]);
-        });
+          var result = [];
+          var type = [];
+          elements.forEach(function(entry){
+              var index = type.indexOf(entry.sensor);
+              if (index == -1) {
+                type.push(entry.sensor);
+                result.push({"key": entry.sensor, "values":[]});
+                index = type.length - 1;
+              }
+              result[index].values.push([entry.timestamp, entry.value]);
+          });
 
-        res.send(result);
+          res.send(result);
+    });
   });
 });
 
@@ -32,17 +34,19 @@ router.get('/last/capture', function(req, res, next) {
   var db = req.db;
   var collection = db.get("sensors");
 
-  collection.find({ sensor:'camera' },
-                  { fields: {timestamp:1, type:1, value:1, _id:0},
-                    sort: {timestamp:-1}, limit: 1 }, function (err, elements) {
+  collection.ensureIndex({ sensor:1 }, { background:true }, function(err, indexName) {
+    collection.find({ sensor:'camera' },
+                    { fields: {timestamp:1, type:1, value:1, _id:0},
+                      sort: {timestamp:-1}, limit: 1 }, function (err, elements) {
 
-          if (elements.length == 0) {
-            res.status(404).send('Not found');
-          }
+            if (elements.length == 0) {
+              res.status(404).send('Not found');
+            }
 
-          var element = elements[0];
-          res.writeHead(200, { 'Content-Type': element.type, 'Cache-Control': 'no-cache' });
-          res.end( new Buffer(element.value, 'base64'), 'binary');
+            var element = elements[0];
+            res.writeHead(200, { 'Content-Type': element.type, 'Cache-Control': 'no-cache' });
+            res.end( new Buffer(element.value, 'base64'), 'binary');
+    });
   });
 });
 
