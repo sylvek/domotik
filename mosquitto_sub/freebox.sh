b2755dd40942552eca56a8dcf0b42f6236782ec8
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-16 20:05:17 +0200

    in fact.. necessary

--- mosquitto_sub/freebox.sh
+++ mosquitto_sub/freebox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 FREEBOX="http://hd1.freebox.fr/pub/remote_control?code=3976946&key="
-mosquitto_sub -t triggers/+/menu | xargs curl -s -L "${FREEBOX}power" &
-mosquitto_sub -t triggers/+/play | xargs curl -s -L "${FREEBOX}ok" &
-mosquitto_sub -t triggers/+/plus | xargs curl -s -L "${FREEBOX}vol_inc" &
-mosquitto_sub -t triggers/+/minus | xargs curl -s -L "${FREEBOX}vol_dec" &
-mosquitto_sub -t triggers/+/forward | xargs curl -s -L "${FREEBOX}prgm_inc" &
-mosquitto_sub -t triggers/+/rewind | xargs curl -s -L "${FREEBOX}prgm_dec" &
+mosquitto_sub -t triggers/+/menu | xargs -I curl -s -L "${FREEBOX}power" &
+mosquitto_sub -t triggers/+/play | xargs -I curl -s -L "${FREEBOX}ok" &
+mosquitto_sub -t triggers/+/plus | xargs -I curl -s -L "${FREEBOX}vol_inc" &
+mosquitto_sub -t triggers/+/minus | xargs -I curl -s -L "${FREEBOX}vol_dec" &
+mosquitto_sub -t triggers/+/forward | xargs -I curl -s -L "${FREEBOX}prgm_inc" &
+mosquitto_sub -t triggers/+/rewind | xargs -I curl -s -L "${FREEBOX}prgm_dec" &
