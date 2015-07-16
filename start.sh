f9071cbed4f1fb9ab939d1fe96f83b4340342874
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-16 17:44:20 +0200

    doesnt work...

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ cd $HOME/domotik/services
 ./service-cc128.py &
 echo "launching mosquitto subscribing..."
 cd $HOME/domotik/mosquitto_sub
-./syslog.sh
 ./mongodb.sh
 ./freebox.sh
 echo "launching web interface..."
