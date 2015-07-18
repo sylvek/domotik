3dd4a76f50cce76194746dcfa4c586795b06a925
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2015-07-18 12:00:33 +0200

    wind doesnt matter

--- cron_juvisy.sh
+++ cron_juvisy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-$HOME/domotik/mosquitto_pub/wind_juvisy.sh &&
 $HOME/domotik/mosquitto_pub/temp_juvisy.sh
--- mosquitto_pub/wind_juvisy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-$HOME/domotik/services/service-yahooWeatherRetriever.py 55863490 wind | mosquitto_pub -t sensors/juvisy/wind -l
