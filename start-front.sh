7950e7dc02164ecadf353414f40360513004e6f7
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2016-01-12 20:37:31 +0100

    eth0

--- start-front.sh
+++ start-front.sh
@@ -4,5 +4,5 @@ cd $HOME/domotik/web
 npm install
 node_modules/bower/bin/bower install
 nohup npm start > /dev/null &
-IP=$(ifconfig wlan0 | awk 'sub(/inet addr:/,""){print $1}')
+IP=$(ifconfig eth0 | awk 'sub(/inet addr:/,""){print $1}')
 echo "go to http://$IP:3000"
