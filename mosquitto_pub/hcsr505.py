c4fe392c4ce2cd13e30eb70c824367e1e6d59c2f
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2016-01-24 14:56:22 +0100

    another approach

--- mosquitto_pub/hcsr505.py
+++ mosquitto_pub/hcsr505.py
@@ -11,24 +11,38 @@ parser.add_argument('hostname', metavar='hostname', help='hostname of mqtt serve
 parser.add_argument('port', metavar='port', help='port of mqtt server', nargs='?', default="1883")
 args = parser.parse_args()
 
+run = True
+
 def signal_handler(signal, frame):
     print "Ending and cleaning up"
     GPIO.remove_event_detect(14)
     GPIO.cleanup()
     client.disconnect()
+    run = False
 
 def event_detected(channel):
     value = GPIO.input(14)
-    client.publish("sensors/hcsr505/motion", value)
+    client.publish("sensors/hcsr505/event", value)
 
-signal.signal(signal.SIGINT, signal_handler)
-client = mqtt.Client()
+try:
+    signal.signal(signal.SIGINT, signal_handler)
+    client = mqtt.Client()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
 
-GPIO.setup(14, GPIO.IN)
-GPIO.add_event_detect(14, GPIO.BOTH, callback=event_detected, bouncetime=100)
+    GPIO.setup(14, GPIO.IN)
+    GPIO.add_event_detect(14, GPIO.BOTH, callback=event_detected, bouncetime=100)
 
-client.connect(args.hostname, int(args.port), 60)
-client.loop_forever()
+    client.connect(args.hostname, int(args.port), 60)
+except:
+	sys.exit(errno.EIO)
+
+while run:
+	try:
+		client.loop()
+        value = GPIO.input(14)
+        client.publish("sensors/hcsr505/motion", value)
+        time.sleep(10)
+	except:
+		sys.exit(errno.EIO)
