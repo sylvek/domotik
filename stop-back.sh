8dfee12aee0adcb7881da6987d71f23793c3ded0
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2016-01-31 21:32:48 +0100

    stop services

--- stop-back.sh
+++ stop-back.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "stopping mosquitto subscribing..."
 pkill mosquitto_sub
+pkill syslog.py
+pkill freebox.py
+pkill led.py
 echo "stopping services..."
-pkill domotik
+pkill service
+echo "stopping publishers..."
+pkill cc128.py
+pkill thn132n.py
+pkill hcsr505.py
 echo "bye"
