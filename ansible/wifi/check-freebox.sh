2220a8d7df99a0681de101e3651067a38ae736e6
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2016-02-11 15:58:19 +0100

    ajout moyenne temperature extérieure par jour

--- ansible/wifi/check-freebox.sh
+++ ansible/wifi/check-freebox.sh
@@ -2,6 +2,11 @@
  if ping -c 1 192.168.0.1 ; then
       echo "Network connection up"
  else
-      echo "Network connection down! Attempting reconnection."
-      sudo /sbin/ifup --force wlan0
+      ps cax | grep ifup > /dev/null
+      if [ $? -eq 1 ]; then
+        echo "Network connection down! Attempting reconnection."
+        sudo /sbin/ifup --force wlan0
+      else
+        echo "wlan0 is already waiting for up."
+      fi
  fi
