1b0adbe0ac9533c1befff1f0bc63459ac3bfec68
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2016-08-05 15:36:55 +0200

    ajout d'un detecteur d'intrustion.

--- services/service-alertDetectInstruction.py
+++ services/service-alertDetectInstruction.py
@@ -2,14 +2,13 @@
 import paho.mqtt.client as mqtt
 import argparse
 import signal
+import datetime
 import smtplib
-import json
-import os.path
 
-parser = argparse.ArgumentParser(description='send a mail if value increased by X %')
+parser = argparse.ArgumentParser(description='detect intrusion')
 parser.add_argument('service_name', metavar='service_name', help='name of the current service')
 parser.add_argument('measure_in', metavar='measure_in', help='measure path given')
-parser.add_argument('percent', metavar='percent', help='10% => 1.10')
+parser.add_argument('nb_hours', metavar='nb_hours', help='ex. 12 for 12hours of latency')
 parser.add_argument('from_mail', metavar='from_mail', help='mail sender')
 parser.add_argument('to_mail', metavar='to_mail', help='mail receiver')
 parser.add_argument('mail_server', metavar='mail_server', help='mail hostname server', nargs='?', default="localhost")
@@ -17,13 +16,11 @@ parser.add_argument('hostname', metavar='hostname', help='hostname of mqtt serve
 parser.add_argument('port', metavar='port', help='port of mqtt server', nargs='?', default="1883")
 args = parser.parse_args()
 
-previous_value = 0
-
-def send_mail(current_value, percent):
+def send_mail():
     header = "From: %s\n" % args.from_mail
     header += "To: %s\n" % args.to_mail
-    header += "Subject: [domotik] consumption increased by %s percent." % str(percent)
-    body = "current value %s should be abnormal. Please check your installation." % str(current_value)
+    header += "Subject: [domotik] possible intrusion detection."
+    body = "no motion was recorded for %s hours, and something is appening right now." % args.nb_hours
     server = smtplib.SMTP(args.mail_server)
     server.sendmail(args.from_mail, args.to_mail, header + body)
 
@@ -31,29 +28,22 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(args.measure_in)
 
 def on_message(client, userdata, msg):
-    global previous_value
-    current_value = float(msg.payload)
-    p = float(args.percent)
-    if (previous_value != 0 and current_value > previous_value * p):
-        send_mail(current_value, current_value * 100 / previous_value)
-    previous_value = current_value
+    current_datetime = datetime.datetime.now()
+    trigger_datetime = locked_datetime + datetime.timedelta(hours=int(args.nb_hours))
+    if msg.payload and trigger_datetime < current_datetime:
+        send_mail()
+    locked_datetime = current_datetime
 
 def signal_handler(signal, frame):
-    with open(__file__ + "." + args.service_name + ".previous", 'w') as outfile:
-        global previous_value
-        json.dump({'previous_value':previous_value}, outfile)
     print "Ending and cleaning up"
     client.disconnect()
 
-if os.path.exists(__file__ + "." + args.service_name + ".previous"):
-    with open(__file__ + "." + args.service_name + ".previous", 'r') as infile:
-        previous_value = json.load(infile)['previous_value']
-
 signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+
+locked_datetime = datetime.datetime.now()
 
 client.connect(args.hostname, int(args.port), 60)
 client.loop_forever()
