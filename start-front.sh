e01b6a931e235931d45067842489b8190a1734f7
Sylvain Maucourt <sylvain.maucourt@deveryware.net>
2016-11-26 14:16:15 +0100

    start/stop front scripts do not start/stop mongodb.py

--- start-front.sh
+++ start-front.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "launching mosquitto subscribing..."
 #/home/pi/domotik/mosquitto_sub/syslog.py $1 &
-/home/pi/domotik/mosquitto_sub/mongodb.py $1 &
+#/home/pi/domotik/mosquitto_sub/mongodb.py $1 &
 
 echo "launching webserver..."
 MONGO_DB='127.0.0.1:27017/domotik' forever start -s /home/pi/domotik/web/bin/www
