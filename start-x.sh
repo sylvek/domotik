17272ba8e87b31ff59c549da332f73998176f378
Sylvain <sylvain.maucourt@edelia.fr>
2017-07-11 23:35:22 +0200

    works as well

--- start-x.sh
+++ start-x.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "launching X..."
-startx -- -nocursor &
-sleep 10 && xset -display :0 -dpms && xset -display :0 dpms 0 0 0 && xset -display :0 s noblank && xset -display :0 s off && xset -display :0 s reset && xset -display :0 s noexpose
+startx -- -nolisten tcp -nocursor -dpms -s 0 &
