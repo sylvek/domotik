65462f3f266370b43f35709acc440fc678b742d6
Sylvain <sylvain.maucourt@edelia.fr>
2017-10-14 15:24:41 +0200

    Google Home

--- web/routes/api.js
+++ web/routes/api.js
@@ -1,6 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/google-home-request', function(req, res, next) {
+  var db = req.db;
+  var result = {};
+
+  res.set('Content-Type', 'application/json');
+
+  var type = req.body.result.parameters.type;
+  var table = ("watt" == type) ? "measures" : "sensors";
+  var sensor = ("watt" == type) ? "sumPerDay" : req.body.result.parameters.sensors;
+  var unit = ("watt" == type) ? " watt" : " degrés";
+
+  var collection = db.get(table);
+  collection.ensureIndex({ type:1, timestamp:1 }, { background:true }, function(err, indexName) {
+    collection.find({ type: type, sensor: sensor },
+                    { fields: {value:1, _id:0},
+                      sort: {timestamp:-1},
+                      limit: 1},
+                      function (err, elements) {
+          result.speech = "aucune valeur trouvée";
+          result.displayText = "aucune valeur trouvée";
+
+          elements.forEach(function(entry){
+                result.speech = entry.value + unit;
+                result.displayText = entry.value + unit;
+          });
+
+          res.send(result);
+    });
+  });
+});
+
 router.get('/last/:time(24h|20s)/:type(temp|watt)', function(req, res, next) {
   var db = req.db;
   var collection = db.get("sensors");
