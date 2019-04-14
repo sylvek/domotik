8a829f02fb8f873479643513d4025b0213703812
Sylvain <sylvain.maucourt@edelia.fr>
2019-04-14 17:40:31 +0200

    remove check for esp12e because at start it takes all the CPU by looping for every messages

--- back/domotik/start-back.sh
+++ back/domotik/start-back.sh
@@ -4,7 +4,7 @@ python /domotik/services/service-calculateMeanPerHour.py domotik_watt_per_hour s
 python /domotik/services/service-calculateSumPerDay.py domotik_watt_per_day measures/meanPerHour/watt measures/sumPerDay/watt current/sumPerDay/watt $1 &
 python /domotik/services/service-persistCurrentValue.py domotik_persist_esp12e sensors/esp12e/temp current/esp12e/temp $1 &
 python /domotik/services/service-persistCurrentValue.py domotik_persist_esp8266 sensors/esp8266/temp current/esp8266/temp $1 &
-python /domotik/services/service-checkSensorsAvailability.py domotik_check_esp12e sensors/esp12e/temp 30 triggers/led/blink e06fb9 $1 &
+#python /domotik/services/service-checkSensorsAvailability.py domotik_check_esp12e sensors/esp12e/temp 30 triggers/led/blink e06fb9 $1 &
 
 trap 'exit 0' SIGINT SIGTERM
 
