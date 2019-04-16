7a6390eafc4144c3e7739d10652557a3920faa5a
Sylvain <sylvain.maucourt@edelia.fr>
2019-04-16 14:34:55 +0200

    new version

--- front/domotik/public/javascripts/domotik-app-current-internal.js
+++ front/domotik/public/javascripts/domotik-app-current-internal.js
@@ -5,7 +5,8 @@
 
     return({
       last: last,
-      getWeather: getWeather
+      getWeather: getWeather,
+      quote: quote
     });
 
     function getWeather(query) {
@@ -41,6 +42,10 @@
       return $http.get("api/last/" + time + "/" + type);
     }
 
+    function quote() {
+      return $http.get("http://quotes.rest/qod.json");
+    }
+
   });
 
   app.controller('domotikIndexCtrl', function($scope, $interval, domotikSrv){
@@ -52,14 +57,12 @@
       $scope.hot_water_comsuption_yesterday = "--";
       $scope.hot_water_mean_last_30_days = "--";
 
-      $scope.temp_outside1 = "--";
-      $scope.moisture_outside1 = "--";
-      $scope.battery_outside1 = "--";
-      $scope.temp_outside2 = "--";
-      $scope.temp_bathroom = "--";
-      $scope.temp_living_room = "--";
+      $scope.temp_outside = "--";
+      $scope.temp_home = "--";
       $scope.power_hour = "--";
       $scope.power_now = "--";
+
+      $scope.quote = "--";
 
       $scope.last_position_timestamp = 0;
 
@@ -72,11 +75,8 @@
                   onSuccess: function() {
                       client.subscribe("current/+/temp");
                       client.subscribe("current/+/watt");
-                      client.subscribe("sharemyposition/+/position");
                       client.subscribe("sensors/+/temp");
                       client.subscribe("sensors/+/watt");
-                      client.subscribe("sensors/+/battery");
-                      client.subscribe("sensors/+/moisture");
                   }
               });
           }
@@ -88,34 +88,15 @@
         switch(categories[1]) {
           // room
           case "esp12e":
-            $scope.temp_outside2 = payload;
+            $scope.temp_outside = payload;
             break;
-          // bathroom
+          // home
           case "esp8266":
-            $scope.temp_bathroom = payload;
+            $scope.temp_home = payload;
             break;
-          // outside
-          case "thn132n":
+          // power
+          case "linky":
             switch (categories[2]) {
-              case "temp":
-                $scope.temp_outside1 = payload;
-                break;
-              case "moisture":
-                $scope.moisture_outside1 = payload;
-                break;
-              case "battery":
-                $scope.battery_outside1 = payload;
-                break;
-              default:
-                break;
-            }
-            break;
-          // living room
-          case "cc128":
-            switch (categories[2]) {
-              case "temp":
-                $scope.temp_living_room = payload;
-                break;
               case "watt":
                 $scope.power_now = (payload / 1000).toPrecision(4);
                 break;
@@ -123,28 +104,13 @@
                 break;
             }
             break;
-          case "cc128mean":
+          case "linkymean":
             $scope.power_hour = (payload / 1000).toPrecision(4);
             break;
           case "sumPerDay":
             $scope.sum_watt_today = (payload / 1000).toPrecision(4);
             break;
           default:
-            switch (categories[2]) {
-              case "position":
-                var p = JSON.parse(payload);
-                if ($scope.last_position_timestamp < p.timestamp) {
-                  var position = p.lat + "," + p.lng;
-                  var newDate = new Date();
-                  newDate.setTime(p.timestamp);
-                  $scope.last_date = newDate.toLocaleString();
-                  $scope.last_position_timestamp = p.timestamp;
-                  $scope.last_position = "http://staticmap.openstreetmap.de/staticmap.php?center=" + position + "&zoom=13&size=400x500&maptype=mapnik&markers=" + position + ",ol-marker-gold"
-                }
-                break;
-              default:
-                break;
-            }
             break;
         }
 
@@ -155,11 +121,8 @@
           console.log("onSuccess => subscribe to sensors, triggers (temp) & positions");
           client.subscribe("current/+/temp");
           client.subscribe("current/+/watt");
-          client.subscribe("sharemyposition/+/position");
           client.subscribe("sensors/+/temp");
           client.subscribe("sensors/+/watt");
-          client.subscribe("sensors/+/battery");
-          client.subscribe("sensors/+/moisture");
         }
       });
 
@@ -167,11 +130,34 @@
         $scope.ephemeride = ephemeris.getTodayEphemeris();
       }
 
-      var weather = function() {
-        //$scope.weather1 = domotikSrv.getWeather("Juvisy,FR");
-        //$scope.weather2 = domotikSrv.getWeather("Santa Monica,CA");
-        //$scope.weather3 = domotikSrv.getWeather("New York,NY");
+      var quote = function() {
+        domotikSrv.quote().then(function(response) {$scope.quote = response.contents.quotes[0].quote});
+      }
 
+      var wallpaper = function() {
+        random = Math.floor((Math.random() * 80) + 1);
+        $scope.background = "img/" + random + ".jpg";
+      }
+
+      var twitter = function() {
+        // twitter
+        // https://github.com/arusahni/ngtweet/issues/30
+        // took too much CPU.
+        $scope.slug = "";
+        var now = new Date();
+        if (now.getDay() > 0 && now.getDay() < 6 && /* week days */
+            now.getHours() > 5 && now.getHours() < 9 /* during departure */)
+        {
+          $scope.slug = "transilien";
+        }
+        if (now.getDay() > 0 && now.getDay() < 6 && /* week days */
+            now.getHours() > 18 && now.getHours() < 23 /* during sunrise */)
+        {
+          $scope.slug = "news";
+        }
+      }
+
+      var weather = function() {
         domotikSrv.last("30d", "sumPerDay").then(function(response) {
           if (response.data.length > 0) {
             var sensor = response.data[0];
@@ -210,55 +196,12 @@
           }
         });
       };
+
       weather();
       ephemeride();
-
-      // twitter
-      // https://github.com/arusahni/ngtweet/issues/30
-      // took too much CPU.
-      $scope.slug = "";
-      var now = new Date();
-      if (now.getDay() > 0 && now.getDay() < 6 && /* week days */
-          now.getHours() > 5 && now.getHours() < 9 /* during departure */)
-      {
-        $scope.slug = "transilien";
-      }
-      if (now.getDay() > 0 && now.getDay() < 6 && /* week days */
-          now.getHours() > 18 && now.getHours() < 23 /* during sunrise */)
-      {
-        $scope.slug = "news";
-      }
-
-      // wallpaper
-      switch (now.getMonth()) {
-        default:
-        case 0:
-          $scope.background = "images/newyear.jpg";
-          break;
-        case 1:
-        case 2:
-          $scope.background = "images/winter.jpg";
-          break;
-        case 3:
-        case 4:
-          $scope.background = "images/motherday.jpg";
-          break;
-        case 5:
-          $scope.background = "images/spring.jpg";
-          break;
-        case 6:
-        case 7:
-        case 8:
-          $scope.background = "images/summer.jpg";
-          break;
-        case 9:
-        case 10:
-          $scope.background = "images/autumn.jpg";
-          break;
-        case 11:
-          $scope.background = "images/christmas.jpg";
-          break;
-      }
+      quote();
+      wallpaper();
+      twitter();
 
       // clock
       var timeController = this;
