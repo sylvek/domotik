cc3a7f7430e485b5ec75b9b4d6d0602a4c793cca
Sylvain <sylvain.maucourt@edelia.fr>
2019-04-16 09:12:27 +0200

    migration to linky

--- back/domotik/start-back.sh
+++ back/domotik/start-back.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "launching services..."
-python /domotik/services/service-calculateMeanPerHour.py domotik_watt_per_hour sensors/cc128/watt sensors/cc128mean/watt measures/meanPerHour/watt $1 &
+python /domotik/services/service-calculateMeanPerHour.py domotik_watt_per_hour sensors/linky/watt sensors/linkymean/watt measures/meanPerHour/watt $1 &
 python /domotik/services/service-calculateSumPerDay.py domotik_watt_per_day measures/meanPerHour/watt measures/sumPerDay/watt current/sumPerDay/watt $1 &
 python /domotik/services/service-persistCurrentValue.py domotik_persist_esp12e sensors/esp12e/temp current/esp12e/temp $1 &
 python /domotik/services/service-persistCurrentValue.py domotik_persist_esp8266 sensors/esp8266/temp current/esp8266/temp $1 &
