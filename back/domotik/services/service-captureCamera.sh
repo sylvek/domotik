7b9ef70036036d2d3f3508b51422a4c5255c7529
Sylvain <sylvain.maucourt@free.fr>
2019-12-22 13:22:29 +0100

    i purged old scripts

--- back/domotik/services/service-captureCamera.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-touch /home/pi/domotik/services/fswebcam.conf
-fswebcam -c /home/pi/domotik/services/fswebcam.conf $1
--- back/domotik/services/service-piTempRetriever.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/opt/vc/bin/vcgencmd measure_temp | grep -o "[0-9.]*"
