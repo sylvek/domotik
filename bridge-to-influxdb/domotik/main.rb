d1c6c9c831973f0137854d3ae45961bc48b29767
Sylvain <sylvain.maucourt@free.fr>
2020-01-06 21:05:52 +0100

    welcome influxdb

---
+++ bridge-to-influxdb/domotik/main.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'mqtt'
+require 'influxdb'
+
+mqtt = MQTT::Client.new ARGV[0]
+influxdb = InfluxDB::Client.new 'domotik', host: ARGV[1], async: true
+
+begin
+    mqtt.connect do |c|
+        c.subscribe('sensors/#', 'measures/#')
+        c.get do |topic, message|
+            elements = topic.split('/')
+            _type = elements[0]
+            _name = elements[1]
+            _unit = elements[2]
+
+            data = {
+                values: { value: message.to_i, type: _type, name: _name, unit: _unit },
+                timestamp: Time.now.to_i
+            }
+            puts data
+            influxdb.write_point(_type, data)
+        end
+    end
+rescue Exception
+    puts "ciao."
+    mqtt.disconnect if mqtt
+end
