9c3653f3b962d7cd9befa5bfa6c719a2836415c5
Sylvain <sylvain.maucourt@free.fr>
2020-01-18 13:44:17 +0100

    mongo => influx + reactjs proto

--- front/domotik/routes/render.js
+++ front/domotik/routes/render.js
@@ -3,16 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'domotik' });
+  res.render('current-internal', { title: 'domotik' });
 });
 router.get('/internal', function(req, res, next) {
   res.render('current-internal', { title: 'domotik' });
-});
-router.get('/external', function(req, res, next) {
-  res.render('current-external', { title: 'domotik' });
-});
-router.get('/history', function(req, res, next) {
-  res.render('history', { title: 'domotik' });
 });
 
 module.exports = router;
