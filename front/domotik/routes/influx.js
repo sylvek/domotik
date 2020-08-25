0471caed6de5e416ff5d14245fe4aefb551533ae
Sylvain Maucourt <sylvain.maucourt@herow.io>
2020-08-25 23:29:41 +0200

    fixes history calculation

--- front/domotik/routes/influx.js
+++ front/domotik/routes/influx.js
@@ -5,21 +5,21 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/last/24h/sumPerDay', function(req, res) {
-    var query = `SELECT median("value") as value FROM "measures" WHERE ("name" = 'sumPerDay' AND time >= now() - 24h) GROUP BY time(24h) fill(none) ORDER BY time DESC LIMIT 1`;
+    var query = `SELECT value as value FROM "infinite"."daily_power_consumption" WHERE time >= now() - 3d ORDER BY time DESC LIMIT 1`;
     influx
         .query(query)
         .then(result => { res.json(result) })
         .catch(err => { res.status(500).send(err.stack) })
 })
 router.get('/last/30d/sumPerDay', function(req, res) {
-    var query = `SELECT median("value") as value FROM "infinite"."daily_power_consumption" WHERE time >= now() - 30d GROUP BY time(30d) fill(none) ORDER BY time DESC LIMIT 1`;
+    var query = `SELECT median(value) as value FROM "infinite"."daily_power_consumption" WHERE time >= now() - 30d ORDER BY time DESC LIMIT 1`;
     influx
         .query(query)
         .then(result => { res.json(result) })
         .catch(err => { res.status(500).send(err.stack) })
 })
 router.get('/last/year/sumPerDay', function(req, res) {
-    var query = `SELECT median("value") as value FROM "infinite"."daily_power_consumption" WHERE time > now() - 370d AND time < now() - 360d GROUP BY time(7d) fill(none) ORDER BY time DESC LIMIT 1`;
+    var query = `SELECT median(value) as value FROM "infinite"."daily_power_consumption" WHERE time > now() - 370d AND time < now() - 360d ORDER BY time DESC LIMIT 1`;
     influx
         .query(query)
         .then(result => { res.json(result) })
