8b45f7adcb682af7ca6fa93b5a05556ed4ddca28
Sylvain Maucourt <sylvain.maucourt@cajoo.eu>
2022-06-09 06:25:06 +0200

    refactoring

--- datastore/domotik/main.go
+++ datastore/domotik/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -13,6 +12,7 @@ import (
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	_ "github.com/glebarez/go-sqlite"
+	database "github.com/sylvek/domotik/datastore/database"
 )
 
 type Log struct {
@@ -22,49 +22,21 @@ type Log struct {
 	value float32
 }
 
-type Aggregation string
-
-const (
-	AVG Aggregation = "AVG"
-	SUM Aggregation = "SUM"
-)
-
-const (
-	WATT string = "watt"
-	TEMP string = "temp"
-)
-
-type Operation struct {
-	aggregate Aggregation
-	from      string
-	to        string
-	unit      string
-}
-
-type Database struct {
-	db              *sql.DB
-	dailyOperations []Operation
-	volatile        bool
-}
-
 func main() {
 	mqttHost := os.Getenv("MQTT_HOST")
 	databasePath := os.Getenv("DB_PATH")
-	log.Printf("starting - MQTT_HOST:%s DB_PATH:%s", mqttHost, databasePath)
+	log.Printf("starting - MQTT_HOST:%s - DB_PATH:%s", mqttHost, databasePath)
 
-	// prepare database
-	databases := make(map[string]*Database)
-	prepareDatabases(databasePath, databases)
+	database := database.NewDatabase(databasePath)
 
-	// connect
-	client, logs := connect(mqttHost, []string{"sensors", "measures"})
+	logs := make(chan Log, 10)
 
-	go treatLogs(logs, databases)
+	client := connect(logs, fmt.Sprintf("tcp://%s:%d", mqttHost, 1883), []string{"sensors", "measures"})
+
+	go treatLogs(logs, database)
 
 	defer func() {
-		for _, v := range databases {
-			v.db.Close()
-		}
+		database.Close()
 		client.Disconnect(250)
 	}()
 
@@ -75,45 +47,27 @@ func main() {
 	log.Println("ciao.")
 }
 
-func prepareDatabases(databasePath string, databases map[string]*Database) {
-	databases["sensors"] = &Database{
-		db:       prepareDatabase(databasePath + "/sensors.db"),
-		volatile: true,
-		dailyOperations: []Operation{
-			{aggregate: AVG, from: "esp12e", to: "daily_temp_outside", unit: TEMP},
-			{aggregate: AVG, from: "esp8266", to: "daily_temp_inside", unit: TEMP}}}
-	databases["measures"] = &Database{
-		db:       prepareDatabase(databasePath + "/measures.db"),
-		volatile: true,
-		dailyOperations: []Operation{
-			{aggregate: SUM, from: "meanPerHour", to: "daily_power_consumption", unit: WATT}}}
-	databases["history"] = &Database{
-		db:       prepareDatabase(databasePath + "/history.db"),
-		volatile: false}
-}
-
-func prepareDatabase(entity string) (db *sql.DB) {
-	db, err := sql.Open("sqlite", entity)
-	if err != nil {
-		panic(err)
+func treatLogs(logs chan Log, database database.Database) {
+	for {
+		l := <-logs
+		err := database.AddSeries(l.kind, l.name, l.unit, l.value)
+		if err != nil {
+			log.Printf(" - error - %s", err)
+			time.Sleep(time.Second)
+			logs <- l
+		}
 	}
-
-	db.Exec("CREATE TABLE IF NOT EXISTS data(id INTEGER PRIMARY KEY AUTOINCREMENT, ts INTEGER, name TEXT, unit TEXT, value REAL);")
-	db.Exec("CREATE INDEX IF NOT EXISTS indx_data on data (ts);")
-
-	return db
 }
 
-func connect(mqttBroker string, kinds []string) (client mqtt.Client, logs chan Log) {
+func connect(logs chan Log, mqttBroker string, kinds []string) (client mqtt.Client) {
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", mqttBroker, 1883))
+	opts.AddBroker(mqttBroker)
 	opts.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
 		payload := string(msg.Payload()[:])
 		elements := strings.Split(msg.Topic(), "/")
 		value, _ := strconv.ParseFloat(payload, 32)
 		logs <- Log{kind: elements[0], name: elements[1], unit: elements[2], value: float32(value)}
 	})
-	logs = make(chan Log, 10)
 	client = mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
@@ -122,56 +76,5 @@ func connect(mqttBroker string, kinds []string) (client mqtt.Client, logs chan L
 	for _, kind := range kinds {
 		client.Subscribe(kind+"/#", 1, nil)
 	}
-	return client, logs
-}
-
-func treatLogs(logs chan Log, databases map[string]*Database) {
-	previousDay := -1
-	for {
-		t := time.Now().UTC()
-		currentDay := t.YearDay()
-
-		if currentDay != previousDay {
-			epoch := t.Unix()
-
-			// we clean old data (meaning older than 2days)
-			for _, v := range databases {
-				if v.volatile {
-					v.db.Exec("DELETE FROM data where ts < ?", epoch-172800 /* 2days */)
-				}
-			}
-
-			// aggregate data
-			var value float64 = -1
-			yesterday := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Add(-24 * time.Hour).Unix()
-			for _, kind := range []string{"sensors", "measures"} {
-				for _, sensorsOperation := range databases[kind].dailyOperations {
-					databases[kind].db.QueryRow(fmt.Sprintf("SELECT %s(value) FROM data WHERE name='%s' AND ts>%d",
-						sensorsOperation.aggregate, sensorsOperation.from, yesterday)).Scan(&value)
-					databases["history"].db.Exec("INSERT INTO data (ts, name, unit, value) VALUES (?, ?, ?, ?)",
-						yesterday,
-						sensorsOperation.to,
-						sensorsOperation.unit,
-						value)
-				}
-				value = -1
-			}
-			previousDay = currentDay
-		}
-
-		item := <-logs
-		log.Printf("%s: %s -> %f (%s)\n", item.kind, item.name, item.value, item.unit)
-
-		db := databases[item.kind].db
-		_, err := db.Exec("INSERT INTO data (ts, name, unit, value) VALUES (?, ?, ?, ?)",
-			t.Unix(),
-			item.name,
-			item.unit,
-			item.value)
-		if err != nil {
-			log.Printf(" - error - %s", err)
-			time.Sleep(time.Second)
-			logs <- item
-		}
-	}
+	return client
 }
