7e1880bd620387ac6f2b235f3f61bcf54e83d114
Sylvain Maucourt <smaucourt@gmail.com>
2022-10-25 19:11:53 +0200

    we subscribe topic during connection

--- datastore/domotik/broker/mqtt.go
+++ datastore/domotik/broker/mqtt.go
@@ -2,8 +2,10 @@ package broker
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
+	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
@@ -18,21 +20,34 @@ type MqttClient struct {
 func (c *MqttClient) ConnectAndListen(logs chan Log) {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(c.mqttBroker)
-	opts.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
+	opts.SetOrderMatters(false)
+	opts.ConnectTimeout = time.Second // Minimal delays on connect
+	opts.WriteTimeout = time.Second   // Minimal delays on writes
+	opts.KeepAlive = 10               // Keepalive every 10 seconds so we quickly detect network outages
+	opts.PingTimeout = time.Second    // local broker so response should be quick
+	opts.ConnectRetry = true
+	opts.AutoReconnect = true
+	opts.OnConnect = func(client mqtt.Client) {
+		log.Println("status: connected")
+		for _, topic := range c.topics {
+			if token := client.Subscribe(topic+"/+/+", 1, nil); token.Wait() && token.Error() != nil {
+				panic(token.Error())
+			}
+			log.Printf("topic [%s] subscribed", topic)
+		}
+	}
+	opts.OnConnectionLost = func(c mqtt.Client, err error) { log.Println("status: connection lost") }
+	opts.OnReconnecting = func(c mqtt.Client, co *mqtt.ClientOptions) { log.Println("status: reconnecting") }
+	opts.DefaultPublishHandler = func(client mqtt.Client, msg mqtt.Message) {
 		payload := string(msg.Payload()[:])
 		elements := strings.Split(msg.Topic(), "/")
 		value, _ := strconv.ParseFloat(payload, 32)
 		logs <- Log{Topic: elements[0], Name: elements[1], Unit: elements[2], Value: float32(value)}
-	})
+	}
+
 	c.client = mqtt.NewClient(opts)
 	if token := c.client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
-	}
-
-	for _, topic := range c.topics {
-		if token := c.client.Subscribe(topic+"/+/+", 1, nil); token.Wait() && token.Error() != nil {
-			panic(token.Error())
-		}
 	}
 }
 
