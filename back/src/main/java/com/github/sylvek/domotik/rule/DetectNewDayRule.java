beca65041cd24e439ec772411d94dda84ca99b6a
Sylvain Maucourt <smaucourt@gmail.com>
2022-10-29 00:18:10 +0200

    reset currentSumPerDayLowPrice + create a measure

--- back/src/main/java/com/github/sylvek/domotik/rule/DetectNewDayRule.java
+++ back/src/main/java/com/github/sylvek/domotik/rule/DetectNewDayRule.java
@@ -26,9 +26,13 @@ public class DetectNewDayRule extends BroadcastableAction {
   public void then(Facts facts) {
     facts.put("currentDay", getDay());
     double currentSumPerDay = facts.get("currentSumPerDay");
+    double lowConsumption = facts.get("currentSumPerDayLowPrice");
+    this.broadcaster.broadcast("measures/sumPerDay/rate",
+        Double.toString(Math.round(lowConsumption / currentSumPerDay * 100.0) / 100.0), false);
     this.broadcaster.broadcast("measures/sumPerDay/watt",
         Long.toString(Math.round(currentSumPerDay * Application.TICK_IN_SECONDS / 3_600.0)), false);
     facts.put("currentSumPerDay", 0d);
+    facts.put("currentSumPerDayLowPrice", 0d);
     facts.put("currentPrice", 0d);
   }
 
