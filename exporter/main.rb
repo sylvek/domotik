d2167012489e4a90011a353ec6b787c1127df3b3
Sylvain Maucourt <smaucourt@gmail.com>
2025-01-20 15:48:20 +0100

    timestamp displayed in the output

--- exporter/main.rb
+++ exporter/main.rb
@@ -6,10 +6,16 @@ db = SQLite3::Database.new ENV['DATABASE']
 name = ENV['NAME'] || 'daily_power_consumption'
 
 db.execute("SELECT ts,value,unit FROM data WHERE name='#{name}' and ts=strftime('%s','now', 'start of day', '-1 day') ORDER BY ts DESC LIMIT 1") do |row|
+    
+    timestamp = row[0]
+    value = row[1]
     unit = row[2]
-    title = "#{row[1].to_i/1000.0} kW.h" if unit == "indice"
-    title = "#{sprintf('%.2f', row[1])} C" if unit == "temp"
-    title = "#{row[1]} L" if unit == "liter"
-    response = HTTParty.get("#{entrypoint}?title=#{title}&timestamp=#{row[0]}")
-    puts response.body
+    
+    title = "#{value.to_i/1000.0} kW.h" if unit == "indice"
+    title = "#{sprintf('%.2f', value)} C" if unit == "temp"
+    title = "#{value} L" if unit == "liter"
+    
+    response = HTTParty.get("#{entrypoint}?title=#{title}&timestamp=#{timestamp}")
+    
+    puts "#{Time.at(timestamp).utc.to_datetime} - #{response.body}"
 end
